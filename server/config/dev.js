1b667d7e06b33e4725a679b35cc7386efd7099ee
= <sasukegc@gmail.com>
2019-01-05 12:05:49 -0200

    adds server

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB_URI: 'mongodb://admin:1234567a@ds243054.mlab.com:43054/bwm-ng-dev',
+  SECRET: 'qhikjiopsqsqhjniop10-303jhiopaf'
+}
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const Rental = require('./models/rental');
+const FakeDb = require('./fake-db');
+const path = require('path');
+
+const rentalRoutes = require('./routes/rentals'),
+      userRoutes = require('./routes/users'),
+      bookingRoutes = require('./routes/bookings');
+
+mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
+  if(process.env.NODE_ENV !== 'production'){ 
+    const fakeDb = new FakeDb();
+    //fakeDb.seedDb();
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/bookings', bookingRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+  const appPath = path.join(__dirname, '..', 'dist/bwm-cli');
+  app.use(express.static(appPath));
+  
+  app.get('*', function(req, res){
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function() {
+  console.log('I am running on port: ' + PORT)
+});
